1888c035e1da7a4c12908e598893a38e2ba7ed27
oskakl767-sys <oskakl767@gmail.com>
2026-07-01 21:45:03 +0300

    sync: copy frontend/src/main/java/com/tawasul/app/data/model/CollectedData.kt

---
+++ frontend/src/main/java/com/tawasul/app/data/model/CollectedData.kt
@@ -0,0 +1,9 @@
+package com.tawasul.app.data.model
+
+import java.io.File
+
+sealed class CollectedData {
+    data class JsonResult(val json: String) : CollectedData()
+    data class FileResult(val file: File, val metadata: String = "") : CollectedData()
+    data class TextResult(val text: String) : CollectedData()
+}
